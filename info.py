6a327cd3a950259575b22389d0ae53c52b1030e1
Akash Shibu <84174571+AKASH1234-bot@users.noreply.github.com>
2026-03-27 16:30:52 +0530

    Refactor environment variable handling in info.py
    Updated environment variable handling and improved code readability.

--- info.py
+++ info.py
@@ -2,84 +2,142 @@ import re
 from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
+
 def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+    if str(value).lower() in ["true", "yes", "1", "enable", "y"]:
         return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+    elif str(value).lower() in ["false", "no", "0", "disable", "n"]:
         return False
     else:
         return default
 
-# Bot information
+
+# ================= BOT INFO ================= #
+
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '4052973'))
 API_HASH = environ.get('API_HASH', '3238bd8ae26df065d11c4054fe8a231c')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7117213451:AAEtfKo7hy7V1WXoH5FKdT65Y_xSBI-kTwQ')
+BOT_TOKEN = environ.get('BOT_TOKEN', 'YOUR_BOT_TOKEN_HERE')  # 🔐 change in env
 
-# Bot settings
+
+# ================= SETTINGS ================= #
+
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://te.legra.ph/file/aa5e35b86c0018c346ca6.jpg')).split()
+PICS = environ.get(
+    'PICS',
+    'https://te.legra.ph/file/aa5e35b86c0018c346ca6.jpg'
+).split()
 
-# Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '707282066 1746132193').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001642260702 -1001591302937 -1001699721730 -1001663686263').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+
+# ================= ADMINS ================= #
+
+ADMINS = [
+    int(admin) if id_pattern.search(admin) else admin
+    for admin in environ.get('ADMINS', '707282066 1746132193').split()
+]
+
+CHANNELS = [
+    int(ch) if id_pattern.search(ch) else ch
+    for ch in environ.get(
+        'CHANNELS',
+        '-1001642260702 -1001591302937 -1001699721730 -1001663686263'
+    ).split()
+]
+
+auth_users = [
+    int(user) if id_pattern.search(user) else user
+    for user in environ.get('AUTH_USERS', '').split()
+]
+
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+
 auth_channel = environ.get('AUTH_CHANNEL', '-1002059503298')
-auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+
+auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+
+
+# ================= REQUIRED CHANNELS ================= #
+
 REQ_CHANNEL_1 = environ.get("REQ_CHANNEL_1", "-1002012736737")
-REQ_CHANNEL_1 = int(REQ_CHANNEL_1) if REQ_CHANNEL_1 and id_pattern.search(REQ_CHANNEL_1) else None
+REQ_CHANNEL_1 = int(REQ_CHANNEL_1) if id_pattern.search(str(REQ_CHANNEL_1)) else None
 
 REQ_CHANNEL_2 = environ.get("REQ_CHANNEL_2", "-1002067561371")
-REQ_CHANNEL_2 = int(REQ_CHANNEL_2) if REQ_CHANNEL_2 and id_pattern.search(REQ_CHANNEL_2) else None
+REQ_CHANNEL_2 = int(REQ_CHANNEL_2) if id_pattern.search(str(REQ_CHANNEL_2)) else None
 
-# Port
-PORT = environ.get("PORT", "8080")
 
-# MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://plotlinethe:24DqkVSpG3ibzcC4@cluster0.048lbou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# ================= PORT ================= #
+
+PORT = int(environ.get("PORT", 8080))
+
+
+# ================= DATABASE ================= #
+
+DATABASE_URI = environ.get(
+    'DATABASE_URI',
+    "mongodb+srv://username:password@cluster.mongodb.net/?retryWrites=true&w=majority"
+)
+
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_Files')
+
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
-# Others
+# ✅ CRITICAL FIX (THIS WAS MISSING)
+FILES_DATABASE = environ.get('FILES_DATABASE', DATABASE_URI)
+
+# already exists but kept safe
+MAX_BTN = int(environ.get('MAX_BTN', 10))
+
+
+# ================= OTHER SETTINGS ================= #
+
 SPELL_LNK = environ.get('SPELL_LNK', 'https://t.me/+PBf0P-7hMh4yZmM1')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001716429576'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
-IMDB = is_enabled((environ.get('IMDB', "False")), False)
-SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "Tru")), True)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "FILE : <code>{file_name}</code> \n \n \n 🔰👉 Subscribe our Youtube Channel to win free Iphone Giveaway🥺👇 ❤️ \n \n https://youtube.com/@ThePlotlinee \n https://youtube.com/@ThePlotlinee \n https://youtube.com/@ThePlotlinee \n \n ------------------------- \n ➧@Cinemaclub_3 \n ➧@Cinemaclub_4 \n ➧@ccllinks</b>")
+
+P_TTI_SHOW_OFF = is_enabled(environ.get('P_TTI_SHOW_OFF', "False"), False)
+IMDB = is_enabled(environ.get('IMDB', "False"), False)
+SINGLE_BUTTON = is_enabled(environ.get('SINGLE_BUTTON', "True"), True)
+
+CUSTOM_FILE_CAPTION = environ.get(
+    "CUSTOM_FILE_CAPTION",
+    "FILE : <code>{file_name}</code>"
+)
+
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", "")
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌‌‌‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
+
+IMDB_TEMPLATE = environ.get(
+    "IMDB_TEMPLATE",
+    "<b>{title}</b> ({year}) ⭐ {rating}"
+)
+
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
+
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
-MAX_BTN = int(environ.get('MAX_BTN', 10))   # ← ADDED
+
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001642260702')).split()]
-MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "False")), False)
-PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
-PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
-LOG_STR = "Current Cusomized Configurations are:-\n"
-LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
-LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
-LOG_STR += ("SINGLE_BUTTON is Found, filename and files size will be shown in a single button instead of two separate buttons\n" if SINGLE_BUTTON else "SINGLE_BUTTON is disabled , filename and file_sixe will be shown as different buttons\n")
-LOG_STR += (f"CUSTOM_FILE_CAPTION enabled with value {CUSTOM_FILE_CAPTION}, your files will be send along with this customized caption.\n" if CUSTOM_FILE_CAPTION else "No CUSTOM_FILE_CAPTION Found, Default captions of file will be used.\n")
-LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_IMDB_DESCRIPTION is disabled , Plot will be shorter.\n")
-LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
-LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
-LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
+FILE_STORE_CHANNEL = [
+    int(ch) for ch in environ.get('FILE_STORE_CHANNEL', '-1001642260702').split()
+]
 
-## EXTRA FEATURES ##
+MELCOW_NEW_USERS = is_enabled(environ.get('MELCOW_NEW_USERS', "False"), False)
+PROTECT_CONTENT = is_enabled(environ.get('PROTECT_CONTENT', "False"), False)
+PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "True"), True)
 
-# URL Shortener #
+
+# ================= URL SHORTENER ================= #
+
 URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'ccshort.site')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '5732c2ccb0386c692ab8d51275118f503a0887a9')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'your_api_key')
 
-# Logo Path
+
+# ================= LOG ================= #
+
 LOGO_PATH = environ.get('LOGO_PATH', 'logo.jpg')
+
+LOG_STR = "Bot configuration loaded successfully ✅"
