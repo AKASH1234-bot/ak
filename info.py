2f49eb646b8428cac59aa144a291f5b3b890a685
Akash Shibu <84174571+AKASH1234-bot@users.noreply.github.com>
2026-03-27 13:57:07 +0530

    Add MAX_BTN configuration for button limit
    Added MAX_BTN configuration to set the maximum number of buttons.

--- info.py
+++ info.py
@@ -15,6 +15,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '4052973'))
 API_HASH = environ.get('API_HASH', '3238bd8ae26df065d11c4054fe8a231c')
 BOT_TOKEN = environ.get('BOT_TOKEN', '7117213451:AAEtfKo7hy7V1WXoH5FKdT65Y_xSBI-kTwQ')
+
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
@@ -35,7 +36,7 @@ REQ_CHANNEL_1 = int(REQ_CHANNEL_1) if REQ_CHANNEL_1 and id_pattern.search(REQ_CH
 REQ_CHANNEL_2 = environ.get("REQ_CHANNEL_2", "-1002067561371")
 REQ_CHANNEL_2 = int(REQ_CHANNEL_2) if REQ_CHANNEL_2 and id_pattern.search(REQ_CHANNEL_2) else None
 
-#Port
+# Port
 PORT = environ.get("PORT", "8080")
 
 # MongoDB information
@@ -57,6 +58,7 @@ IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌‌‌�
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+MAX_BTN = int(environ.get('MAX_BTN', 10))   # ← ADDED
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
 FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001642260702')).split()]
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "False")), False)
@@ -74,9 +76,8 @@ LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 ## EXTRA FEATURES ##
-    
-      # URL Shortener #
 
+# URL Shortener #
 URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'ccshort.site')
 URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '5732c2ccb0386c692ab8d51275118f503a0887a9')
 
